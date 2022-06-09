000df5e8f853e55a13db5c114b7c63b23da6a80b
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-09 17:07:42 +0300

    Update Contents.swift
    değişken tipleri

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -5,15 +5,44 @@ import UIKit
 //snake_case
 //camelCase
 
+//String
+
 var userName = "James"
+userName.append("o")
+userName.lowercased()
+userName.uppercased()
+
 var userSurname = "Hetfield"
 print(userName, userSurname)
 
 
 userSurname = "Lars" // var değişkenlerinin değerleri sonradan değiştirilebilir.
 
-let userAge = 30
+let userAge = 30.0
 
 // userAge = 50 yazamayız. HATA!
 
 let pi = 3.14
+
+pi * userAge //işleme sokulan değişkenlerin türleri aynı olmalıdır. double * int olmaz!
+
+
+
+//integer & double & float
+
+//integer
+let userAge2 = 50
+let myNumber = 4
+userAge2 / myNumber
+
+//double
+let userAgeD = 50.0
+let myNumberD = 4.0
+userAgeD / myNumberD
+
+
+//boolean
+var myBoolean = false
+myBoolean = true
+
+
