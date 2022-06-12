20b2a8438dd394df3e8bcff37c9a93416c16ce30
Fulya Sariyar <fulyasariyar@outlook.com>
2022-06-12 17:26:58 +0300

    .

---
+++ İfControl.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var myAge = 32
+if myAge < 30 {
+    print("30 -")
+}
+// AND &&
+//OR ||
+else  if myAge > 30 && myAge < 40{
+    print("30s")
+} else{
+    print("40 +")
+}
+ 3 < 5 && 5 < 7 //&& sağ taraf ve sol tarafda doğru olmalı true olması için
+
+
+3 < 5 || 8 < 7 // || sağ yada sol tarafın tekinin doğru olması yeterli true olması için
+ var myString = "James"
+
+if myString == "James"{
+    print("yes")
+}
+
